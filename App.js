58f4e04347ef31c53473dc15df2fd44ace114a58
vsaterdinova <vsaterdinova.exceedteam@gmail.com>
2021-12-17 18:27:37 +0300

    create database

---
+++ App.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors')
+const mongoose = require("mongoose");
+const app = express();
+
+const { Schema } = mongoose;
+
+const logShema = new Schema({
+    login: String,
+    password: String
+});
+
+const reсShema = new Schema({
+    name: String,
+    doctor: String,
+    data: String,
+    complaint: String
+});
+
+const Reception = mongoose.model("reseptions", reсShema);
+const User = mongoose.model('users', logShema)
+
+const url = 'mongodb+srv://ValeriaSaterdinova:restart987*@education.cssf9.mongodb.net/Hospital?retryWrites=true&w=majority';
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.use(express.json());
